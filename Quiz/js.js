c2d89df8ebd6c259b997e09db5f9cad2a8e51ed6
vincentdyland <vincentdyland@gmail.com>
2021-11-23 01:57:52 +0700

    Upload part 2

--- Quiz/js.js
+++ Quiz/js.js
@@ -1,39 +1,36 @@
-const quizData = [{
-    question: 'What your boyfriend?',
-    a: 'John',
-    b: 'Bob',
-    c: 'Thinh HandSome Boy',
-    d: 'Doe',
-    correctAnswer: 'c'
-}, {
-    question: 'How old is he?',
-    a: '12',
-    b: '13',
-    c: '20',
-    d: '19',
-    correctAnswer: 'c'
-}, {
-    question: 'Are you the type of person?',
-    a: 'Beautiful girl',
-    b: 'Messy',
-    c: 'Lazy',
-    d: 'All of them',
-    correctAnswer: 'd'
-}, {
-    question: 'Do you take a bath?',
-    a: 'Not too much',
-    b: 'Yes, I do',
-    c: 'Never',
-    d: 'Sometime',
-    correctAnswer: 'd'
-}, {
-    question: 'What is the brand of your laptop?',
-    a: 'Lenovo',
-    b: 'Hp',
-    c: 'Dell',
-    d: 'MacBook',
-    correctAnswer: 'c'
+const quizData = [
+   {question: 'What your boyfriend?', a: 'John', b: 'Bob', c: 'Thinh HandSome Boy', d: 'Doe', correctAnswer: 'c'
+
+}, {question: 'How old is he?', a: '12', b: '13', c: '20', d: '19', correctAnswer: 'c'
+
+}, {question: 'Are you the type of person?', a: 'Beautiful girl', b: 'Messy', c: 'Lazy', d: 'All of them', correctAnswer: 'd'
+
+}, {question: 'Do you take a bath?', a: 'Not too much', b: 'Yes, I do', c: 'Never', d: 'Sometime', correctAnswer: 'd'
+
+}, {question: 'What is the brand of your laptop?', a: 'Lenovo', b: 'Hp', c: 'Dell', d: 'MacBook', correctAnswer: 'c'
+
+}, {question: 'What is color of the leaves?', a: 'Green', b: 'Yellow', c: 'Blue', d: 'Red', correctAnswer: 'a'
+
+}, {question: 'What is most color of body\'s tree?', a: 'Green', b: 'Yellow', c: 'Blue', d: 'Brown', correctAnswer: 'd'
+
+}, {question: 'What is color of the water?', a: 'Green', b: 'None', c: 'white', d: 'Red', correctAnswer: 'b'
+
+}, {question: 'What is color of cherry?', a: 'Green', b: 'Yellow', c: 'Blue', d: 'Red', correctAnswer: 'd'
+
+}, {question: 'What is color of the orange fruit?', a: 'Green', b: 'Yellow', c: 'orange', d: 'Red', correctAnswer: 'c'
+
+}, {question: 'What is the famous brand of the phone?', a: 'Iphone', b: 'SamSung', c: 'Oppo', d: 'Nokia', correctAnswer: 'a'
+
+}, {question: 'What type of your laptop?', a: 'Dell', b: 'Hp', c: 'Lenovo', d: 'Macbook', correctAnswer: 'b'
+
+}, {question: 'What keeps you cool in the summer in the room?', a: 'Air conditioning', b: 'dryer', c: 'fridge', d: 'fan', correctAnswer: 'a'
+
+}, {question: 'What helps you to charge your phone when you don\'t have an outlet?', a: 'small music', b: 'speaker', c: 'airpods', d: 'close the door', correctAnswer: 'c'
+
+}, {question: 'What is the famous brand of the phone?', a: 'USB drive', b: 'calculator', c: 'power outlet', d: 'backup charger', correctAnswer: 'd'
+
 }]
+
 const quiz = document.getElementById('quiz')
 const answerEls = document.querySelectorAll('.answer')
 const questionEl = document.getElementById('question')
